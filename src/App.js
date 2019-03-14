8f61ebcddfad1227e29a08f47bcbd1d443eef1cf
Ji Shin <j2yshin@gmail.com>
2019-03-14 19:23:05 -0400

    Build messenger section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import './App.scss';
 
-import NavBar from './components/NavBar'
-import Home from './containers/Home'
+import { connect } from 'react-redux';
+import NavBar from './components/NavBar';
+import Home from './containers/Home';
+import Messenger from './components/Messenger'
+
+const faker = require('faker')
+const name = faker.name.findName();
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <NavBar />
-        <Home />
+        <div className='main'>
+          <Home />
+
+        </div>
+        <Messenger faker={faker}/>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    home: state.home
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setHome: (data) => dispatch({type: 'SET_HOME', payload: data})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/Messenger.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+let users = []
+
+class Messenger extends Component {
+
+  render() {
+
+    for(let i=0;i<25;i++) {
+      users.push(
+        <div className='user'>
+          <img className='m-avatar' src={this.props.faker.image.image()} />
+          {this.props.faker.name.firstName() + ' ' + this.props.faker.name.lastName()}
+        </div>)
+    }
+
+    return (
+      <div className='messages'>
+        {
+        users.map(user => user)
+        }
+      </div>
+    );
+  }
+
+}
+
+export default Messenger;
